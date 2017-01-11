1f08e53737a563461b0ecb0aeffd098b96b55ccd
David Christiansen <david@davidchristiansen.dk>
2017-01-11 13:42:03 -0500

    Improve test coverage

--- test.rkt
+++ test.rkt
@@ -5,8 +5,13 @@
 (define (f x arg)
   (+ (add1 x) 3))
 
+(check-equal? (f 3 'whatever) 7)
+
 (define foo
   (lambda args (cons 1 args)))
 
 (check-equal? (map (λ (x) (add1 x)) '(1 2 3 4))
               '(2 3 4 5))
+
+(check-equal? (map foo '(a b c))
+              '((1 a) (1 b) (1 c)))
