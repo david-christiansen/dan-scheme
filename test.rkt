d8ddec34df6102361f13c14bc34c009a92a182df
David Christiansen <david@davidchristiansen.dk>
2017-01-11 13:54:56 -0500

    Add tests for errors

--- test.rkt
+++ test.rkt
@@ -1,6 +1,21 @@
 #lang dan-scheme
-
+(require (prefix-in r: racket)
+         (for-syntax
+          (only-in racket
+                   quasisyntax unsyntax
+                   #%app #%datum #%top
+                   quote syntax)
+          (prefix-in r: racket/base)))
 (require rackunit)
+
+(r:define-syntax (convert-syntax-error stx)
+  (r:syntax-case stx ()
+    [(_ expr)
+     (r:with-handlers ([r:exn:fail:syntax?
+                      (r:lambda (e)
+                        #`(r:error '#,(r:exn-message e)))])
+       (r:parameterize ((r:error-print-source-location #f))
+         (r:local-expand #'expr 'expression r:null)))]))
 
 (define (f x arg)
   (+ (add1 x) 3))
@@ -15,3 +30,25 @@
 
 (check-equal? (map foo '(a b c))
               '((1 a) (1 b) (1 c)))
+
+(check-exn r:exn:fail?
+           (λ ()
+             (convert-syntax-error
+              (lambda args (if (pair? args) #t #f)))))
+
+(check-exn r:exn:fail?
+           (λ ()
+             (convert-syntax-error
+              (lambda args (append args args)))))
+
+(check-exn r:exn:fail?
+           (λ ()
+             (convert-syntax-error
+              (define (f . args) args))))
+
+(check-exn r:exn:fail?
+           (λ ()
+             (convert-syntax-error
+              (define-syntax (thing stx)
+                (r:syntax-case stx ()
+                  ((_ x) #'x))))))
