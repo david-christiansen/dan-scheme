b360234644e13cfdec97eee02f201f876d847181
Ben Greenman <benjaminlgreenman@gmail.com>
2017-01-17 13:33:33 -0500

    propagate source locations in error messages
    Changes:
    - runtime errors print their line number
    - `dan-define` and `dan-lambda` attach the correct source locations
    - added a fancy-and-brittle macro to test that error messages
      give approximately the right source location

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "dan-scheme")
 (define version "0.1")
-(define deps '("base"))
+(define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/dan-scheme.scrbl" ())))
 (define pkg-desc "A very little language for situations where simplicity is desired over convenience")
