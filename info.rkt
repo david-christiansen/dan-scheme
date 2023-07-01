97ec8a2666a315d8a79a360692e0101cf6d175b4
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-07-01 21:13:56 +0100

    Update info.rkt add "Little language" category to documentation index page

--- info.rkt
+++ info.rkt
@@ -3,5 +3,5 @@
 (define version "0.1")
 (define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/dan-scheme.scrbl" ())))
+(define scribblings '(("scribblings/dan-scheme.scrbl" () ("Little language"))))
 (define pkg-desc "A very little language for situations where simplicity is desired over convenience")
